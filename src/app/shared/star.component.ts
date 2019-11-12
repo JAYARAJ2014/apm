0e98a092d9ad80d5c57bae4c9e30779c0b7d32c0
Jayaraj Sivadasan <jayaraj.sivadasan@gmail.com>
2019-11-12 17:56:09 -0500

    Added @Input @Output decorators, events for nested component

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges, Input } from '@angular/core';
+import { Component, OnChanges, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector : 'pm-star',
@@ -10,9 +10,18 @@ export class StartComponent implements OnChanges {
 
   @Input() rating ;
   starWidth: number;
+  @Output() ratingClicked: EventEmitter<string> =
+  new EventEmitter<string>();
+
 
   ngOnChanges(): void {
       this.starWidth = this.rating * 75 / 5;
   }
+
+  onClick(): void {
+    this.ratingClicked.emit(`${this.rating}`)
+  }
+
+
 
 }
