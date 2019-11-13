3760835e7222312302aad988c5ea59ea17c0c1bd
Jayaraj Sivadasan <jayaraj.sivadasan@gmail.com>
2019-11-12 21:22:34 -0500

    Created Product Service, Injected to root and invoked in product list

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -16,7 +16,7 @@ export class StartComponent implements OnChanges {
 
   ngOnChanges(): void {
       this.starWidth = this.rating * 75 / 5;
-  }
+  } 
 
   onClick(): void {
     this.ratingClicked.emit(`${this.rating}`)
