70002d91f5ea118a897ae23956719cc41983c171
Jayaraj Sivadasan <jayaraj.sivadasan@gmail.com>
2019-11-14 22:52:46 -0500

    Added parameterized route

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -8,7 +9,21 @@ import { IProduct } from './product';
 export class ProductDetailComponent implements OnInit {
   product: IProduct;
   pageTitle = 'Product Detail';
-  constructor() {}
+  constructor(private route: ActivatedRoute) {
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += ` : ${id}`;
+    this.product =  {
+      productId: 1,
+      productName: 'Leaf Rake',
+      productCode: 'GDN-0011',
+      releaseDate: 'March 19, 2019',
+      description: 'Leaf rake with 48-inch wooden handle.',
+      price: 19.95,
+      starRating: 3.2,
+      imageUrl: 'assets/images/leaf_rake.png'
+    };
+  }
 }
